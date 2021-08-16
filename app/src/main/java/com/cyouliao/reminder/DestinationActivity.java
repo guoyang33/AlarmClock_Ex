cf4772e37f2ec3040b3ea27695250dc24aa1d2ee
guoyang33 <guoyang33@gmail.com>
2021-08-16 12:09:29 +0800

    update 1100816

---
+++ app/src/main/java/com/cyouliao/reminder/DestinationActivity.java
@@ -0,0 +1,13 @@
+package com.cyouliao.reminder;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class DestinationActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_destination);
+    }
+}
